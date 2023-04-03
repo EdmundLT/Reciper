d299fe0481ee51c61cdd4a029a9320813f303af2
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2023-04-02 22:48:42 -0400

    deployment

---
+++ Reciper/Migrations/20230402225622_Recipe2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reciper.Migrations
+{
+    public partial class Recipe2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
